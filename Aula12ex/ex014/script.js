cfb802e3fd55c13537674229ec7131270c585cfb
Leonardo Gama <leogama09@gmail.com>
2022-02-07 16:01:40 -0300

    Terminei o exercício ex014

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora < 12) {
+        // BOM DIA!
+        img.src ='fotomanhã.png'
+        document.body.style.background = '#fed091'
+    } else if (hora >= 12 && hora <= 18) {
+        // B0A TARDE!
+        img.src ='fototarde.png'
+        document.body.style.background = '#df8f5a'
+    } else {
+        img.src ='fotonoite.png'
+        document.body.style.background = '#172c4a'
+        // BOA NOITE!
+    }
+}
